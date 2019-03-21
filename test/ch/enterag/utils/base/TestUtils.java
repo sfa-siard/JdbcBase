fb2eb720a753a86c0c6977b94b30f68338354ccf
Hartwig Thomas <hartwig.thomas@enterag.ch>
2019-03-21 17:42:06 +0100

    2.1.34

--- test/ch/enterag/utils/base/TestUtils.java
+++ test/ch/enterag/utils/base/TestUtils.java
@@ -1,11 +1,13 @@
 package ch.enterag.utils.base;
 
 import java.io.*;
+import java.util.*;
 
 import ch.enterag.utils.SU;
 
 public abstract class TestUtils
 {
+  private static Random _random = new Random(47);
   public static byte[] getBytes(int iLength)
   {
     byte[] buf = new byte[iLength];
@@ -85,4 +87,89 @@ public abstract class TestUtils
     return sw.getBuffer().toString();
   } /* getNString */
 
+  public static int getRandomInteger()
+  {
+    return _random.nextInt();
+  }
+  
+  public static short getRandomShort()
+  {
+    return (short)(_random.nextInt() & 0x0000FFFF);
+  }
+  
+  public static long getRandomLong()
+  {
+    return _random.nextLong();
+  }
+  
+  public static String getRandomFixedString(int iLength)
+  {
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < iLength; i++)
+    {
+      int j = (int)(96*_random.nextDouble());
+      sb.appendCodePoint(32+j);
+    }
+    return sb.toString();
+  }
+  
+  public static String getRandomString(int iMaxLength)
+  {
+    int iLength = (int)(iMaxLength*_random.nextDouble());
+    return getRandomFixedString(iLength);
+  }
+  
+  public static String getRandomFixedNString(int iLength)
+  {
+    /* generate random bytes in the range [x20,xFF] */
+    byte[] buf = new byte[iLength];
+    for (int i = 0; i < iLength; i++)
+    {
+      int j = (int)(192*_random.nextDouble());
+      if (j < 96)
+        buf[i] = (byte)(32+j);
+      else
+        buf[i] = (byte)(64+j);
+    }
+    /* read them into a string as 1252 */
+    return SU.getIsoLatin1String(buf);
+  }
+  
+  public static String getRandomNString(int iMaxLength)
+  {
+    int iLength = (int)(iMaxLength*_random.nextDouble());
+    return getRandomFixedNString(iLength);
+  }
+  
+  public static float getRandomFloat()
+  {
+    return _random.nextFloat();
+  }
+  
+  public static double getRandomDouble()
+  {
+    return _random.nextDouble();
+  }
+  
+  public static java.sql.Date getRandomDate()
+  {
+    long lCentury = 365L*100L;
+    long lTime = 1000L*60L*60L*24L*(long)(lCentury*_random.nextDouble());
+    return new java.sql.Date(lTime);
+  }
+
+  public static java.sql.Time getRandomTime()
+  {
+    long lDay = 1000L*60L*60L*24L;
+    long lTime = (long)(lDay*_random.nextDouble());
+    return new java.sql.Time(lTime);
+  }
+  
+  public static java.sql.Timestamp getRandomTimestamp()
+  {
+    long lCentury = 1000L*60L*60L*24L*365L*100L;
+    long lTime = (long)(lCentury*_random.nextDouble());
+    return new java.sql.Timestamp(lTime);
+  }
+  
 }
